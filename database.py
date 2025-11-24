5d3e644e4ae8d2e8b6ead23cca95178a9f33b2da
sanji <1f0rccee@gmail.com>
2025-11-24 21:59:23 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,29 @@
+# database.py
+
 import json
 import os
 
 DB_FILE = "users.json"
 
-# Загрузка базы
-if os.path.exists(DB_FILE):
-    with open(DB_FILE, "r", encoding="utf-8") as f:
-        USERS = json.load(f)
-else:
-    USERS = {}
+# username -> { "role": "...", "desc": "...", "banner": "banners/xxx.jpg" }
+USERS = {}
 
-# Функции для сохранения
+
+def load_db():
+    """Загрузить базу пользователей из файла."""
+    global USERS
+    if os.path.exists(DB_FILE):
+        with open(DB_FILE, "r", encoding="utf-8") as f:
+            USERS = json.load(f)
+    else:
+        USERS = {}
+
+
 def save_db():
+    """Сохранить базу пользователей в файл."""
     with open(DB_FILE, "w", encoding="utf-8") as f:
         json.dump(USERS, f, indent=4, ensure_ascii=False)
+
+
+# Загружаем базу при импорте модуля
+load_db()
